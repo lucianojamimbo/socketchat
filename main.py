524db559cb72ece9d326bb90271a8b9b6650debc
lucianojamimbo <lucianojamimbo@gmail.com>
2020-08-07 12:36:27 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,60 @@
+import socket
+import threading
+
+#define shit:
+S_IP = "0.0.0.0"
+S_PORT = 25565
+HEADERSIZE = 10
+IP = input("Enter peer IP")
+PORT = 25565
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+r = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#send setup:
+def estabsend():
+    global clientsocket
+    s.bind((S_IP, S_PORT))
+    s.listen(5)
+    clientsocket, address = s.accept()
+    print("connection from {0} has been established!".format(address))
+    msg = "Connection established"
+    msg = f'{len(msg):<{HEADERSIZE}}' + msg
+    clientsocket.send(bytes(msg, "utf-8"))
+def send():
+    msg = input("Input message to send to clients: ")
+    msg = f'{len(msg):<{HEADERSIZE}}' + msg
+    clientsocket.send(bytes(msg, "utf-8"))
+
+#receive setup:
+def requestconnection():
+    print("Trying to connect...")
+    r.connect((IP, PORT))
+    print("Connection successful")
+
+def receive():
+    HEADERSIZE = 10
+    full_msg = ''
+    new_msg = True
+    msglen = 0
+    while True:
+        msg = r.recv(16)
+        if new_msg:
+            msglen = int(msg[:HEADERSIZE])
+            new_msg = False        
+        full_msg += msg.decode("utf-8")
+        if len(full_msg)-HEADERSIZE == msglen:
+            print("\nServer says: ", full_msg[HEADERSIZE:])
+            new_msg = True
+            full_msg = ''
+
+#run:
+est_thread = threading.Thread(target=estabsend)
+estrecv_thread = threading.Thread(target=requestconnection)
+est_thread.start()
+estrecv_thread.start()
+est_thread.join()
+estrecv_thread.join()
+receive = threading.Thread(target=receive)
+receive.start()
+while True:
+    send()
